cfdb67dd6adafb3668fc462ddc5878f563f345c3
Kamal <kamalshcs@gmail.com>
2026-04-05 23:31:35 +0530

    Added project card code with dynamic variables

--- script.js
+++ script.js
@@ -1,30 +1,44 @@
 const projects = [
   {
+    imageURL: "images/card-image1.png",
     badge: "Web Design",
     title: "Optimizing Data Collection with Forms",
     description: "This redesign not only solved a critical usability problem but also demonstrated the value of a user-centered, data-driven approach, paving the way for a more intuitive and efficient platform.",
-    imageURL: "images/card-image1.png",
     link: "#"
   },
   {
+    imageURL: "images/card-image2.png",
     badge: "Web Design",
     title: "Redefining the Announcements Experience",
     description: "This redesign of the Announcements module stands as a testament to the power of user-centered design combined with strategic business objectives.",
-    imageURL: "images/card-image1.png",
     link: "#"
   },
   {
+    imageURL: "images/card-image3.png",
     badge: "Wireframing",
     title: "Core HR Platform Landing Page",
     description: "This project reinforced the importance of balancing aesthetic appeal with clear, concise communication, especially for a complex B2B product.",
-    imageURL: "images/card-image1.png",
     link: "#"
   },
   {
+    imageURL: "images/card-image4.svg",
     badge: "Product Design ",
     title: "Revamping the Goals Module",
     description: "This project demonstrates my ability to not only enhance UI/UX but also build a complex, feature-rich front-end application from the ground up using Angular.",
-    imageURL: "images/card-image1.png",
     link: "#"
   }
 ];
+
+const projectContainer = document.getElementById('project-list');
+
+const projectCard = `<article class="project-card-glass">
+                        <div class="card-image-container">
+                            <img class="card-images" src="${projects.imageURL}" alt="Description of Projects">
+                            <span class="category-badge">${projects.badge}</span>
+                        </div>
+                        <div class="card-content">
+                            <h3>${projects.title}</h3>
+                            <p>${projects.descriptions}</p>
+                            <button type="button" class="view-project-btn">View Project</button>
+                        </div>
+                    </article>`;
