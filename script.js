bcc2f9be7b6707038bc26a646947dc6be7645c5c
Kamal <kamalshcs@gmail.com>
2026-04-05 23:18:07 +0530

    Created a javascript array with projects

---
+++ script.js
@@ -0,0 +1,30 @@
+const projects = [
+  {
+    badge: "Web Design",
+    title: "Optimizing Data Collection with Forms",
+    description: "This redesign not only solved a critical usability problem but also demonstrated the value of a user-centered, data-driven approach, paving the way for a more intuitive and efficient platform.",
+    imageURL: "images/card-image1.png",
+    link: "#"
+  },
+  {
+    badge: "Web Design",
+    title: "Redefining the Announcements Experience",
+    description: "This redesign of the Announcements module stands as a testament to the power of user-centered design combined with strategic business objectives.",
+    imageURL: "images/card-image1.png",
+    link: "#"
+  },
+  {
+    badge: "Wireframing",
+    title: "Core HR Platform Landing Page",
+    description: "This project reinforced the importance of balancing aesthetic appeal with clear, concise communication, especially for a complex B2B product.",
+    imageURL: "images/card-image1.png",
+    link: "#"
+  },
+  {
+    badge: "Product Design ",
+    title: "Revamping the Goals Module",
+    description: "This project demonstrates my ability to not only enhance UI/UX but also build a complex, feature-rich front-end application from the ground up using Angular.",
+    imageURL: "images/card-image1.png",
+    link: "#"
+  }
+];
